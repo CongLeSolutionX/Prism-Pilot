a95cb2f683121a128ffb872b80343b2769b53fc8
Cong Le <cle155@csu.fullerton.edu>
2025-04-11 09:45:17 -0700

    initial version

---
+++ Prism_Pilot/Prism_Pilot/Prism_PilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prism_PilotApp.swift
+//  Prism_Pilot
+//
+//  Created by Cong Le on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Prism_PilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
